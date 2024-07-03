fb2de3b56f4a3d2a16ee9d6071bf986d06d94942
to1dev <admin@to1.dev>
2024-07-03 10:53:20 +0200

    Update dev info and fix github profile link

--- src/lib/components/Realm.svelte
+++ src/lib/components/Realm.svelte
@@ -44,7 +44,7 @@
     class="flex items-center justify-center min-h-screen bg-gray-200 dark:bg-gray-800 background"
 >
     <div
-        class="container m-4 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3 shadow-2xl transform duration-200 easy-in-out border-2 border-white rounded-t-xl"
+        class="container m-4 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3 shadow-2xl transform duration-100 easy-in-out border-2 border-white rounded-t-xl"
     >
         <div class="h-48 overflow-hidden rounded-t-xl">
             {#if profile}
@@ -232,7 +232,7 @@
                         {#if typeof profile.l.w[0] === "string"}
                             <a
                                 href="https://{profile.l.w[0]}"
-                                class="hover:text-neutral-400"
+                                class="hover:text-neutral-400 duration-100"
                                 target="_blank"
                                 ><svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -286,10 +286,36 @@
                             ></a
                         >
                     {/if}
+                    {#if profile.l.g}
+                        <a
+                            href="https://github.com/{profile.l.g}"
+                            class="hover:text-neutral-400 duration-100"
+                            target="_blank"
+                            ><svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+                                ><path
+                                    stroke="none"
+                                    d="M0 0h24v24H0z"
+                                    fill="none"
+                                /><path
+                                    d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+                                /></svg
+                            ></a
+                        >
+                    {/if}
                     {#if profile.l.t}
                         <a
                             href="https://t.me/{profile.l.t}"
-                            class="hover:text-neutral-400"
+                            class="hover:text-neutral-400 duration-100"
                             target="_blank"
                             ><svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -315,7 +341,7 @@
                     {#if profile.l.d}
                         <a
                             href="https://discord.gg/{profile.l.d}"
-                            class="hover:text-neutral-400"
+                            class="hover:text-neutral-400 duration-100"
                             target="_blank"
                             ><svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -345,7 +371,7 @@
                     {#if profile.p.n}
                         <a
                             href="npub:{profile.p.n}"
-                            class="hover:text-neutral-400"
+                            class="hover:text-neutral-400 duration-100"
                             target="_blank"
                             ><svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -380,7 +406,7 @@
             <hr class="mt-6" />
             <div class="flex bg-gray-50">
                 <div
-                    class="flex justify-center items-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer duration-200"
+                    class="flex justify-center items-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer duration-100"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -409,7 +435,7 @@
                 </div>
                 <div class="border"></div>
                 <div
-                    class="flex justify-center items-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer duration-200"
+                    class="flex justify-center items-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer duration-100"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -444,6 +470,16 @@
             by
             <a href="https://x.com/to1dev" target="_blank" class="ml-1"
                 >@to1dev</a
+            >
+            <a href="https://github.com/to1dev/arc20me" target="_blank" class="ml-2"
+                ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 496 512"
+                    class="w-4 h-4 hover:fill-neutral fill-gray-400 duration-100"
+                    ><path
+                        d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                    /></svg
+                ></a
             >
         </div>
         {#if debug && realmData}
