0fdfbeaaacc13005d7d748a90cd1f11bfedcd8fa
to1dev <admin@to1.dev>
2024-07-05 13:10:48 +0200

    temporarily remove server api functions

---
+++ src/lib/components/realm/parts/Debug.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let realmData;
+</script>
+
+<div class="flex flex-col justify-center items-center p-4 bg-white border-t">
+    {#if realmData}
+        <div class="mt-2 font-light leading-relaxed break-all">
+            {JSON.stringify(realmData, null, 4)}
+        </div>
+    {/if}
+</div>
