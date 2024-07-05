b795d6d9320d5947390f8bbf35ccfd68f63b1332
to1dev <admin@to1.dev>
2024-07-05 07:56:43 +0200

    update ui fonts

--- src/lib/components/realm/DK.svelte
+++ src/lib/components/realm/DK.svelte
@@ -36,7 +36,7 @@
 
 <div class="flex items-center justify-center min-h-screen">
     <div
-        class="container m-4 sm:w-full md:w-2/3 2xl:w-2/5 xl:w-3/5 lg:w-1/2 shadow-2xl transform border-2 border-white rounded-t-xl"
+        class="container m-4 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3 shadow-2xl transform border-2 border-white rounded-t-xl"
     >
         <div class="h-48 overflow-hidden rounded-t-xl">
             <img
@@ -185,7 +185,7 @@
                     <Title name={profile?.n} realm={realmData?.realm} />
                 {/if}
                 <p
-                    class="mt-2 text-gray-500 font-light leading-relaxed sm:text-xl text-lg"
+                    class="mt-2 text-base-100 font-light leading-relaxed"
                 >
                     {#if profile?.d?.enUS}
                         {DOMPurify.sanitize(profile.d.enUS)}
@@ -430,9 +430,7 @@
             </div>
         </div>
 
-        <div
-            class="flex justify-center items-center p-4 bg-white border-t"
-        >
+        <div class="flex justify-center items-center p-4 bg-white border-t">
             Bring to you with <span class="text-red-500 mx-1 emoji">❤️</span>
             by
             <a href="https://x.com/to1dev" target="_blank" class="ml-1"
