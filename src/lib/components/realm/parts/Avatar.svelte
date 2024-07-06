40e5e2a71b81beab6af1b12a8a06fdc476f67b95
to1dev <admin@to1.dev>
2024-07-06 21:09:35 +0200

    update components layouts and error handler

--- src/lib/components/realm/parts/Avatar.svelte
+++ src/lib/components/realm/parts/Avatar.svelte
@@ -3,17 +3,20 @@
 
     export let image;
 
-    /*let random: number | null = null;
+    onMount(async () => {});
 
-    onMount(async () => {
-        random = Math.floor(Math.random() * 30) + 1;
-    });*/
+    let fallbackImage = "/images/loading/animals/24.gif";
+    function handleImageError(event: Event) {
+        const target = event.target as HTMLImageElement;
+        target.src = fallbackImage;
+    }
 </script>
 
 <div class="flex justify-center px-5 -mt-12 bg-white">
     <img
         class="h-32 w-32 bg-white p-2 rounded-full"
-        src={image ? image : `/images/loading/animals/24.gif`}
+        src={image ? image : fallbackImage}
         alt=""
+        on:error={handleImageError}
     />
 </div>
--- src/lib/components/realm/parts/Debug.svelte
+++ src/lib/components/realm/parts/Debug.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     export let meta;
+
+    $: highlightedJson = JSON.stringify(meta, null, 4);
 </script>
 
-<div class="flex flex-col justify-center items-center p-4 bg-white border-t bottom-0">
+<div
+    class="flex flex-col justify-center items-center p-4 bg-white border-t bottom-0"
+>
     {#if meta}
         <div class="mt-2 font-light leading-relaxed break-all">
-            {JSON.stringify(meta, null, 4)}
+            <pre><code class="json">{highlightedJson}</code></pre>
         </div>
     {/if}
 </div>
