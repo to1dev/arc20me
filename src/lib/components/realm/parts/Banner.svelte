be163b70a55bb8ccdfb2d5c3f1ced82773e5bf97
to1dev <admin@to1.dev>
2024-07-10 19:40:26 +0200

    rollback Banner img layout

--- src/lib/components/realm/parts/Banner.svelte
+++ src/lib/components/realm/parts/Banner.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     export let banner;
+
+    //const c1 = "h-48 overflow-hidden flex items-center justify-center";
+    //const c2 = "w-full h-auto object-cover";
 </script>
 
-<div class="h-48 overflow-hidden flex items-center justify-center">
-    <img class="w-full h-auto object-cover" src={banner ? banner : "/images/banner.jpg"} alt="" />
+<div class="h-48 overflow-hidden">
+    <img class="w-full" src={banner ? banner : "/images/banner.jpg"} alt="" />
 </div>
