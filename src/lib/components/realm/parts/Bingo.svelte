15b05e78a177e29d0c30f400c4afb4ea8c7b8d18
to1dev <admin@to1.dev>
2024-07-10 21:39:52 +0200

    update global font

--- src/lib/components/realm/parts/Bingo.svelte
+++ src/lib/components/realm/parts/Bingo.svelte
@@ -26,15 +26,13 @@
         >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-shovel"
+                class="w-5 h-5 icon icon-tabler icons-tabler-outline icon-tabler-shovel"
                 ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
                     d="M17 4l3 3"
                 /><path d="M18.5 5.5l-8 8" /><path
