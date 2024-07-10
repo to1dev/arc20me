4d921c91e7d4e1b200842536eeeeece0f53b527f
to1dev <admin@to1.dev>
2024-07-10 17:24:23 +0200

    test production env

--- src/lib/components/realm/parts/Content.svelte
+++ src/lib/components/realm/parts/Content.svelte
@@ -2,18 +2,21 @@
     import DOMPurify from "dompurify";
     import { isDk } from "$lib/stores/Realm";
 
+    let _seq = import.meta.env.PERSISTENT_SEQUENCE;
+    console.log(_seq);
+
     export let text;
 </script>
 
 <div class="mt-2 text-gray-600 leading-relaxed">
     {#if $isDk}
         {#if text?.enUS}
-        <div class="m-2">
+            <div class="m-2">
                 {DOMPurify.sanitize(text.enUS)}
             </div>
         {/if}
         {#if text?.zhCN}
-        <div class="m-2">
+            <div class="m-2">
                 {DOMPurify.sanitize(text.zhCN)}
             </div>
         {/if}
