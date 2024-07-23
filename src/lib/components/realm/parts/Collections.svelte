1e8b8bb32f33ebf4188fc7e87c806a81e8cb84c3
to1dev <admin@to1.dev>
2024-07-23 02:50:29 +0200

    remove unused packages and update card styles for Collections component

--- src/lib/components/realm/parts/Collections.svelte
+++ src/lib/components/realm/parts/Collections.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { parseAtomicalIdfromURN } from "$lib/utils/ponytail";
-    import { afterUpdate, onMount } from "svelte";
+    import { afterUpdate } from "svelte";
     import Swiper from "swiper/bundle";
     import "swiper/swiper-bundle.css";
 
@@ -23,15 +23,50 @@
     <div class="overflow-hidden p-2">
         <div id="swiper" class="swiper p-4">
             <div class="swiper-wrapper">
-                {#each Object.entries(collections) as [_, value]}
+                {#each Object.entries(collections) as [_, value], i}
                     <div class="swiper-slide ring-4 ring-white">
                         <div
                             class="card image-full w-full h-full shadow-xl bg-opacity-5"
                         >
-                            <div class="card-body">
-                                <p class="font-light text-white">
+                            <div class="card-body p-3">
+                                <p
+                                    class="text-sm font-light text-white playwrite-fr-moderne"
+                                >
                                     {value?.name}
                                 </p>
+                                <div class="card-actions justify-end">
+                                    <button
+                                        class="btn btn-ghost btn-circle"
+                                        on:click={() => console.log(i)}
+                                        ><svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            stroke-width="2"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-zoom-scan"
+                                            ><path
+                                                stroke="none"
+                                                d="M0 0h24v24H0z"
+                                                fill="none"
+                                            /><path
+                                                d="M4 8v-2a2 2 0 0 1 2 -2h2"
+                                            /><path
+                                                d="M4 16v2a2 2 0 0 0 2 2h2"
+                                            /><path
+                                                d="M16 4h2a2 2 0 0 1 2 2v2"
+                                            /><path
+                                                d="M16 20h2a2 2 0 0 0 2 -2v-2"
+                                            /><path
+                                                d="M8 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"
+                                            /><path d="M16 16l-2.5 -2.5" /></svg
+                                        ></button
+                                    >
+                                </div>
                             </div>
                             <figure>
                                 <img
