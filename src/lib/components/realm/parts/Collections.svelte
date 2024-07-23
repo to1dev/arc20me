c30df03c96771217fa020ee03a66897ef5c216ea
to1dev <admin@to1.dev>
2024-07-23 03:08:12 +0200

    update card styles for Collections component

--- src/lib/components/realm/parts/Collections.svelte
+++ src/lib/components/realm/parts/Collections.svelte
@@ -21,7 +21,7 @@
 
 {#if collections}
     <div class="overflow-hidden p-2">
-        <div id="swiper" class="swiper p-4">
+        <div id="swiper" class="block relative w-60 h-80 p-4 mx-auto z-auto">
             <div class="swiper-wrapper">
                 {#each Object.entries(collections) as [_, value], i}
                     <div class="swiper-slide ring-4 ring-white">
@@ -85,12 +85,7 @@
 
 <style>
     .card.image-full:before {
-        --tw-bg-opacity: 0.15;
-    }
-
-    .swiper {
-        width: 240px;
-        height: 320px;
+        --tw-bg-opacity: 0;
     }
 
     .swiper-slide {
@@ -98,48 +93,5 @@
         align-items: center;
         justify-content: center;
         border-radius: 18px;
-        font-size: 22px;
-        font-weight: bold;
-        color: #fff;
-    }
-
-    .swiper-slide:nth-child(1n) {
-        background-color: rgb(206, 17, 17);
-    }
-
-    .swiper-slide:nth-child(2n) {
-        background-color: rgb(0, 140, 255);
-    }
-
-    .swiper-slide:nth-child(3n) {
-        background-color: rgb(10, 184, 111);
-    }
-
-    .swiper-slide:nth-child(4n) {
-        background-color: rgb(211, 122, 7);
-    }
-
-    .swiper-slide:nth-child(5n) {
-        background-color: rgb(118, 163, 12);
-    }
-
-    .swiper-slide:nth-child(6n) {
-        background-color: rgb(180, 10, 47);
-    }
-
-    .swiper-slide:nth-child(7n) {
-        background-color: rgb(35, 99, 19);
-    }
-
-    .swiper-slide:nth-child(8n) {
-        background-color: rgb(0, 68, 255);
-    }
-
-    .swiper-slide:nth-child(9n) {
-        background-color: rgb(218, 12, 218);
-    }
-
-    .swiper-slide:nth-child(10n) {
-        background-color: rgb(54, 94, 77);
     }
 </style>
