c2bf1fcd72a99664c5eac364bf1415fbf0b3e60d
to1dev <admin@to1.dev>
2024-07-27 13:52:15 +0200

    add more Links icons

--- src/lib/components/realm/parts/Links.svelte
+++ src/lib/components/realm/parts/Links.svelte
@@ -177,6 +177,44 @@
                             /><path
                                 d="M4.9 19a22 22 0 0 1 -.9 -7v-1a8 8 0 0 1 12 -6.95"
                             /></svg
+                        >{:else if item?.type === "instagram"}<svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="w-5 h-5 text-white icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
+                            ><path
+                                stroke="none"
+                                d="M0 0h24v24H0z"
+                                fill="none"
+                            /><path
+                                d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"
+                            /><path
+                                d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"
+                            /><path d="M16.5 7.5l0 .01" /></svg
+                        >{:else if item?.type === "threads"}<svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="w-5 h-5 text-white icon icon-tabler icons-tabler-outline icon-tabler-brand-threads"
+                            ><path
+                                stroke="none"
+                                d="M0 0h24v24H0z"
+                                fill="none"
+                            /><path
+                                d="M19 7.5c-1.333 -3 -3.667 -4.5 -7 -4.5c-5 0 -8 2.5 -8 9s3.5 9 8 9s7 -3 7 -5s-1 -5 -7 -5c-2.5 0 -3 1.25 -3 2.5c0 1.5 1 2.5 2.5 2.5c2.5 0 3.5 -1.5 3.5 -5s-2 -4 -3 -4s-1.833 .333 -2.5 1"
+                            /></svg
                         >{:else}<svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
