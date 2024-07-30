be9cef5c85069b6ba221e018fd3e04bfa2a16900
to1dev <admin@to1.dev>
2024-07-30 10:01:09 +0200

    fix Content dompurify

--- src/lib/components/realm/parts/Content.svelte
+++ src/lib/components/realm/parts/Content.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import DOMPurify from "isomorphic-dompurify";
+    import { onMount } from "svelte";
 
-    export let text;
+    export let text: string | null = null;
 
-    const clean = DOMPurify.sanitize(text);
+    let clean: string | null = null;
+    if (text) {
+        clean = DOMPurify.sanitize(text);
+    }
 </script>
 
 <div class="mt-2 text-slate-500 font-light leading-relaxed">
     <div class="m-2">
-        {@html clean}
+        {#if clean}
+            {@html clean}
+        {/if}
     </div>
 </div>
 
