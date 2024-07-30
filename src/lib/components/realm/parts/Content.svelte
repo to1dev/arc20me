d07ee7539f3be342fe662f65ec9906ea159f8c1b
to1dev <admin@to1.dev>
2024-07-30 09:18:31 +0200

    update Content component

--- src/lib/components/realm/parts/Content.svelte
+++ src/lib/components/realm/parts/Content.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-    import { onMount, onDestroy } from "svelte";
-    import DOMPurify from "isomorphic-dompurify";
-
     export let text;
 </script>
 
 <div class="mt-2 text-slate-500 font-light leading-relaxed">
     <div class="m-2">
         {#if text}
-            {@html DOMPurify.sanitize(text)}
+            {@html text}
         {/if}
     </div>
 </div>
--- src/lib/components/realm/themes/Base.svelte
+++ src/lib/components/realm/themes/Base.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import DOMPurify from "isomorphic-dompurify";
     import Database from "$lib/components/db/Database.svelte";
     import LocalServer from "$lib/components/server/LocalServer.svelte";
 
@@ -51,7 +52,7 @@
         <div class="bg-white pt-3">
             <div class="text-center px-14 break-words">
                 <Title name={profile?.name} realm={realmData?.realm} {uname} />
-                <Content text={profile?.desc} />
+                <Content text={DOMPurify.sanitize(profile?.desc)} />
                 <Bingo {meta} />
                 <NoProfile {meta} />
             </div>
