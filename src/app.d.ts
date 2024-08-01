3e3fe14e6be2887cddc8b055c2cf4a2199d1511d
to1dev <admin@to1.dev>
2024-08-01 20:13:14 +0200

    add service worker conf

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,11 @@ declare global {
         // interface PageData {}
         // interface PageState {}
         // interface Platform {}
+        interface Platform {
+            env: Env;
+            cf: CfProperties;
+            ctx: ExecutionContext;
+        }
     }
 }
 
