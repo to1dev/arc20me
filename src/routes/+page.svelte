d3d88ccb81b02280d1150183b514ca6d580d49f2
to1dev <admin@to1.dev>
2024-08-03 11:39:13 +0200

    fix homepage data-theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Home from "$lib/components/Home.svelte";
 </script>
 
-<div class="flex flex-col lg:flex-row flex-auto min-h-screen">
+<div class="flex flex-col lg:flex-row flex-auto min-h-screen" data-theme="business">
     <div class="flex-auto">
         <div class="mx-auto w-full">
             <div class="space-y-5">
